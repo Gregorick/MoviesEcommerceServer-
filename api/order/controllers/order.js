3e2928dc5bb515325c0db566a25528c45a8e937f
Gregorick <gregorick.liriano@gmail.com>
2022-09-19 17:32:47 -0400

    add order collections

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -1,8 +1,43 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(
+  "sk_test_51LNdqiIFQ2n9baB6p3KDN8AQzWvLk881lDusm5xiVh8iLy4W8byEWZU3g5GAKWgq5511ixlHCepoCfBfisX5YIuA00LLdhhtgE"
+);
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    const { token, products, idUser, addressShipping } = ctx.request.body;
+    let totalPayment = 0;
+    products.forEach((product) => {
+      totalPayment = totalPayment + product.price;
+    });
+
+    const charge = await stripe.charge.create({
+      amount: totalPayment * 100,
+      currency: "eur",
+      source: token.id,
+      description: `ID Usuario: ${idUser}`,
+    });
+    const createOrder = [];
+    for await (const product of products) {
+      const data = {
+        game: product.id,
+        user: idUser,
+        totalPayment,
+        idPayment: charge.id,
+        addressShipping,
+      };
+      const validData = await createStrapi.entityValidator.validateEntity(
+        strapi.models.order,
+        data
+      );
+      const entry = await strapi.query("order").create(validData);
+      createOrder.push(entry);
+    }
+    return createOrder;
+  },
+};
